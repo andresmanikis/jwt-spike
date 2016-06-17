1b1a89934ebe4de11d84d522394435a5758349db
Andrés Manikis <andresmanikis@gmail.com>
2016-06-17 18:45:19 -0300

    Creates authorized-resource but still not authorizes it

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const app = express();
 
 app.use(express.static('static'));
 
+app.get('/api/authorized-resource', (req, res) => {
+  res.end('You made it!');
+});
+
 app.listen(3000, () => console.log('Listening on port 3000...'));
