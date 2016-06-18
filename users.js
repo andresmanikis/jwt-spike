7860de8f3ce776f589c96214bb98c96cf17ac630
Andrés Manikis <andresmanikis@gmail.com>
2016-06-18 17:28:19 -0300

    Adds another user and updates the instructions

--- users.js
+++ users.js
@@ -1,5 +1,6 @@
 const users = [
-  { name: 'John Doe', username: 'john.doe', password: 'j0hnd03' }
+  { name: 'John Doe', username: 'john.doe', password: 'j0hnd03' },
+  { name: 'Jack Black', username: 'jack.black', password: 'j4ck13' }
 ];
 
 function getByUsername(username) {
